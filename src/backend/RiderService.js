2022bbfbf7268898200f022e4d1ec09ba395ae62
kumarnivi <nivethikashivakumar56@gmail.com>
2023-07-09 19:14:01 +0530

    backend crud

--- src/backend/RiderService.js
+++ src/backend/RiderService.js
@@ -1,36 +1,37 @@
-// import axios from 'axios';
+import axios from 'axios';
 
-// class RiderService {
-//   static serverURL = 'http://localhost:9000';
+class RiderService {
+  static serverURL = 'http://localhost:9000';
 
-//   //get all Rider
-//   static getAllRiders() {
-//     const dataURL = `${this.serverURL}/Rider`;
-//     return axios.get(dataURL);
-//   }
-//  //get single Rider
-// static getRider(riderId) {
-//     const dataURL = `${this.serverURL}/Rider/${riderId}`;
-//     return axios.get(dataURL);
-//   }
+  //get all Rider
+  static getAllRiders() {
+    const dataURL = `${this.serverURL}/Rider`;
+    return axios.get(dataURL);
+  }
+ //get single Rider
+static getRider(riderId) {
+    const dataURL = `${this.serverURL}/Rider/${riderId}`;
+    return axios.get(dataURL);
+  }
   
-//   //create Rider
-//   static createRider(add) {
-//     const dataURL = `${this.serverURL}/Rider`;
-//     return axios.post(dataURL, add);
-//   }
-//   //update Rider
-//   static updateRider(add, riderId){
-//     const dataURL = `${this.serverURL}/Rider/${riderId}`;
-//     return axios.put(dataURL, add);
+  //create Rider
+  static createRider(add) {
+    const dataURL = `${this.serverURL}/Rider`;
+    return axios.post(dataURL, add);
+  }
+  //update Rider
+  static updateRider(add, riderId){
+    const dataURL = `${this.serverURL}/Rider/${riderId}`;
+    return axios.put(dataURL, add);
 
-//   }
-//   static deleteRider(riderId) {
-//     const dataURL = `${this.serverURL}/Rider/${riderId}`; // Replace "/api/riders" with the appropriate endpoint URL
-//     return axios.delete(dataURL);
-//   }
-// }
+  }
+  static deleteRider(riderId) {
+    const dataURL = `${this.serverURL}/Rider/${riderId}`; // Replace "/api/riders" with the appropriate endpoint URL
+    return axios.delete(dataURL);
+  }
+}
   
 
 
-// export default RiderService;
+export default RiderService;
+
--- src/componets/Spinner/Spinner.js
+++ src/componets/Spinner/Spinner.js
@@ -1,14 +1,14 @@
-// import React from "react";
-// import SpinnerImg from '../../assets/image/spinning-loading.gif'
+import React from "react";
+import SpinnerImg from '../../assets/image/spinning-loading.gif'
 
-// let Spinner = () => {
+let Spinner = () => {
    
-//     <React.Fragment>
-//         <div>
-//             <img src={SpinnerImg} className="d-block m-auto" alt=""></img>
-//         </div>
-//     </React.Fragment>
+    <React.Fragment>
+        <div>
+            <img src={SpinnerImg} className="d-block m-auto" alt=""></img>
+        </div>
+    </React.Fragment>
         
-// }
+}
 
-// export default Spinner;
+export default Spinner;
